e5bfa053b62eadac96e7f65d25314d52d61d2f90
Alberto Nidasio <alberto.nidasio@skywarder.eu>
2023-03-30 16:49:05 +0200

    [KittCar] Fixed error in BouncingShiftRegister that was reintroduced in a previous commit

--- KittCar/KittCar.srcs/sources/new/BouncingShiftRegister.vhd
+++ KittCar/KittCar.srcs/sources/new/BouncingShiftRegister.vhd
@@ -34,26 +34,28 @@ begin
             dout      <= (0 => '1', others => '0');
             direction <= '0';
             started   <= '0';
-        elsif rising_edge(clk) then
-            if REGISTER_WIDTH /= 1 then
-                -- If we are going right and we have not reaced position 0, keep going right
-                -- If we are going left and the bit is at the end, revert direction
-                if (direction = '0' and reg(0) = '0') or (direction = '1' and reg(REGISTER_WIDTH - 1) = '1') then
-                    reg       <= '0' & reg(REGISTER_WIDTH - 1 downto 1); -- Right shif
-                    direction <= '0';
-                else
-                    reg       <= reg(REGISTER_WIDTH - 2 downto 0) & '0'; -- Left shift
-                    direction <= '1';
-                    started   <= '1';
+        else
+            if rising_edge(clk) then
+                if REGISTER_WIDTH /= 1 then
+                    -- If we are going right and we have not reaced position 0, keep going right
+                    -- If we are going left and the bit is at the end, revert direction
+                    if (direction = '0' and reg(0) = '0') or (direction = '1' and reg(REGISTER_WIDTH - 1) = '1') then
+                        reg       <= '0' & reg(REGISTER_WIDTH - 1 downto 1); -- Right shif
+                        direction <= '0';
+                    else
+                        reg       <= reg(REGISTER_WIDTH - 2 downto 0) & '0'; -- Left shift
+                        direction <= '1';
+                        started   <= '1';
+                    end if;
                 end if;
             end if;
-        end if;
 
-        if falling_edge(clk) then
-            if started = '1' or reg(0) = '1' then
-                dout <= reg;
-            else
-                dout <= (others => '0');
+            if falling_edge(clk) then
+                if started = '1' or reg(0) = '1' then
+                    dout <= reg;
+                else
+                    dout <= (others => '0');
+                end if;
             end if;
         end if;
     end process;
