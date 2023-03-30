c29083c663f74304283a8cc20e59655def29d3c8
Alberto Nidasio <alberto.nidasio@skywarder.eu>
2023-03-30 15:13:19 +0200

    [KittCar] Created simple test bench for KittCarPWM

--- KittCar/KittCar.srcs/sim/new/tb_KittCarPWM.vhd
+++ KittCar/KittCar.srcs/sim/new/tb_KittCarPWM.vhd
@@ -1,54 +1,73 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.all;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity tb_KittCarPWM is
 end tb_KittCarPWM;
 
 architecture tb_KittCarPWM_arch of tb_KittCarPWM is
-    constant REGISTER_WIDTH   : positive := 8;
-    constant TAIL_LENGTH      : positive := 4;
-    constant MAIN_CLK_DIV_THR : positive := 29;
-    constant PWM_CLK_DIV_THR  : positive := 1;
+    constant MASTER_PERIOD        : time     := 10ns;
+    constant RESET_WND            : time     := MASTER_PERIOD * 10;
 
-    constant MASTER_PERIOD    : time     := 10 ns;
-    constant RESET_WND        : time     := MASTER_PERIOD;
+    constant CLK_PERIOD_NS        : positive := 10;
+    constant MIN_KITT_CAR_STEP_MS : positive := 1;
+    constant PWM_CLK_PERIOD_US    : positive := 100;
+    constant NUM_OF_SWS           : positive := 16;
+    constant NUM_OF_LEDS          : positive := 8;
+    constant TAIL_LENGTH          : positive := 4;
+    constant MAIN_CLK_DIV_WIDTH   : positive := 32;
+    constant PWM_CLK_DIV_WIDTH    : positive := 16;
 
-    component KittCar is
+    component KittCarPWM is
         generic (
-            REGISTER_WIDTH     : positive := 8;
-            TAIL_LENGTH        : positive := 4;
-            MAIN_CLK_DIV_WIDTH : positive := 24;
-            MAIN_CLK_DIV_THR   : natural  := 9999999;
-            PWM_CLK_DIV_WIDTH  : positive := 10;
-            PWM_CLK_DIV_THR    : natural  := 999
+            CLK_PERIOD_NS        : positive range 1 to 100  := 10;  -- External clk period in nanoseconds
+            MIN_KITT_CAR_STEP_MS : positive range 1 to 2000 := 1;   -- Minimum step period in milliseconds
+            PWM_CLK_PERIOD_US    : positive range 1 to 1000 := 100; -- PWM clk period in microsend
+
+            NUM_OF_SWS           : integer range 1 to 16    := 16;  -- Number of input switches
+            NUM_OF_LEDS          : integer range 1 to 16    := 16;  -- Number of output LEDs
+
+            TAIL_LENGTH          : integer range 1 to 16    := 16;  -- Tail length
+
+            MAIN_CLK_DIV_WIDTH   : positive                 := 32;
+            PWM_CLK_DIV_WIDTH    : positive                 := 16
         );
         port (
-            reset      : in std_logic;
-            master_clk : in std_logic;
-            pwm_out    : out std_logic_vector(0 to REGISTER_WIDTH - 1)
+            -- Reset and clock
+            reset        : in std_logic;
+            external_clk : in std_logic;
+
+            -- LEDs/SWs
+            sw           : in std_logic_vector(NUM_OF_SWS - 1 downto 0);  -- Switches avaiable on Basys3
+            pwm_out      : out std_logic_vector(NUM_OF_LEDS - 1 downto 0) -- LEDs avaiable on Basys3
         );
     end component;
 
-    signal reset      : std_logic;
-    signal master_clk : std_logic := '1';
-    signal pwm_out    : std_logic_vector(0 to REGISTER_WIDTH - 1);
+    signal reset        : std_logic;
+    signal external_clk : std_logic                                 := '1';
+    signal sw           : std_logic_vector(NUM_OF_SWS - 1 downto 0) := (others => '0');
+    signal pwm_out      : std_logic_vector(NUM_OF_LEDS - 1 downto 0);
 begin
     -- Device under test
-    dut : KittCar
+    dut : KittCarPWM
     generic map(
-        REGISTER_WIDTH   => REGISTER_WIDTH,
-        TAIL_LENGTH      => TAIL_LENGTH,
-        MAIN_CLK_DIV_THR => MAIN_CLK_DIV_THR,
-        PWM_CLK_DIV_THR  => PWM_CLK_DIV_THR
+        CLK_PERIOD_NS        => CLK_PERIOD_NS,
+        MIN_KITT_CAR_STEP_MS => MIN_KITT_CAR_STEP_MS,
+        PWM_CLK_PERIOD_US    => PWM_CLK_PERIOD_US,
+        NUM_OF_SWS           => NUM_OF_SWS,
+        NUM_OF_LEDS          => NUM_OF_LEDS,
+        TAIL_LENGTH          => TAIL_LENGTH,
+        MAIN_CLK_DIV_WIDTH   => MAIN_CLK_DIV_WIDTH,
+        PWM_CLK_DIV_WIDTH    => PWM_CLK_DIV_WIDTH
     )
     port map(
-        reset      => reset,
-        master_clk => master_clk,
-        pwm_out    => pwm_out
+        reset        => reset,
+        external_clk => external_clk,
+        sw           => sw,
+        pwm_out      => pwm_out
     );
 
     -- Clock generation
-    master_clk <= not master_clk after MASTER_PERIOD/2;
+    external_clk <= not external_clk after MASTER_PERIOD/2;
 
     -- Reset generation
     reset_wave : process
